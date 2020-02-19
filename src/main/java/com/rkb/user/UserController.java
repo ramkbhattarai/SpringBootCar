5007e3ff7b1315339baba2a4f04559919661b672
Ram <ramkb466@gmail.com>
2020-02-19 12:24:55 -0500

    added update for user

--- src/main/java/com/rkb/user/UserController.java
+++ src/main/java/com/rkb/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,11 @@ public class UserController {
 	
 	@PostMapping
 	public User saveUser(@RequestBody User u) {
+		return userService.upsert(u);
+	}
+	
+	@PutMapping("/{id}")
+	public User updateUser(@RequestBody User u) {
 		return userService.upsert(u);
 	}
 	
