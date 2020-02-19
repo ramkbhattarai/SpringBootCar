09e3e016321790f4be8ce2c393094ce2a10510b9
Ram <ramkb466@gmail.com>
2020-02-19 10:08:50 -0500

    initial commit

---
+++ src/main/java/com/rkb/CarUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.rkb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarUserApiApplication.class, args);
+	}
+
+}
